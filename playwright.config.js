4d6b1892e70ca47fe8e530ec2f4ae1d3ad61da5b
Vortex1806 <shubhvora03@gmail.com>
2026-05-26 23:07:19 +0530

    Updated to add better test reporting logging and video saving to s3

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,22 @@
-const { defineConfig } = require('@playwright/test');
+const { defineConfig } = require("@playwright/test");
 
 module.exports = defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
 
   timeout: 30000,
 
+  retries: 1,
+
+  outputDir: "test-results",
+
   use: {
     headless: true,
-    viewport: { width: 1280, height: 720 }
+    viewport: { width: 1280, height: 720 },
+
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+    trace: "retain-on-failure",
   },
 
-  reporter: [
-    ['list'],
-    ['html', { open: 'never' }]
-  ]
+  reporter: [["list"], ["html", { open: "never" }]],
 });
